45b3bbb72f4542377142f1775dfe39cfc98b82d2
Greg Lockwood <gregl@clearboxsystems.com.au>
2014-12-02 17:55:21 +1100

    Fixed all issues

--- public/js/controller.js
+++ public/js/controller.js
@@ -1,20 +1,33 @@
-//f Managing the poll list
-function PollListCtrl($scope) {
-  $scope.polls = [];
-}
-// Voting / viewing poll results
-function PollItemCtrl($scope, $routeParams) {
-  $scope.poll = {};
-  $scope.vote = function() {};
-}
-// Creating a new poll
-function PollNewCtrl($scope) {
-  $scope.poll = {
-    question: '',
-    choices: [{ text: '' }, { text: '' }, { text: '' }]
-  };
-  $scope.addChoice = function() {
-  $scope.poll.choices.push({ text: '' });
-  };
-  $scope.createPoll = function() {};
-  }
+(function () {
+    var app = angular.module('polls');
+
+    //f Managing the poll list
+    app.controller('PollListCtrl', function PollListCtrl($scope) {
+        $scope.polls = [];
+    });
+    // Voting / viewing poll results
+    app.controller('PollItemCtrl', function PollItemCtrl($scope, $routeParams) {
+        $scope.poll = {};
+        $scope.vote = function () {};
+    });
+    // Creating a new poll
+    app.controller('PollNewCtrl', function PollNewCtrl($scope) {
+        $scope.poll = {
+            question: '',
+            choices: [{
+                text: ''
+            }, {
+                text: ''
+            }, {
+                text: ''
+            }]
+        };
+        $scope.addChoice = function () {
+            $scope.poll.choices.push({
+                text: ''
+            });
+        };
+        $scope.createPoll = function () {};
+    });
+
+})();
